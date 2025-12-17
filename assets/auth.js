b2207a93647bb1097f94cffbcbbf14334acc90ef
CiwAI Smart Digital <ciwaismartdigital@gmail.com>
2025-12-17 19:10:24 +0700

    edit: ruangujian login index

--- assets/auth.js
+++ assets/auth.js
@@ -1,3 +1,7 @@
+// true = mode tamu (login NIK OFF)
+// false = mode login NIK ON
+const GUEST_MODE = true;
+
 const SHEET_URL =
     "https://docs.google.com/spreadsheets/d/e/2PACX-1vSTursHTu2TvoC3xxU_TLR3oPj-pogr6eCadRyIlQVUomxISw9Ipu2Nv6HoFSTp7QT8XXM_aF83zJ_v/pub?gid=1812990548&single=true&output=csv";
 
