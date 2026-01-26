3b6ff7ca7f04c91458e842927c08a3c3c1942971
Ciwai-lab <ciwaismartdigital@gmail.com>
2026-01-26 21:13:37 +0700

    init: tenant js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -78,3 +78,9 @@ window.executeLogin = async function () {
         msg.innerText = "Gagal koneksi ke server";
     }
 };
+
+setTimeout(() => {
+    const modal = document.getElementById("loginModal");
+    if (modal) document.body.appendChild(modal);
+}, 50);
+
